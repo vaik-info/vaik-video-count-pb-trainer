752bd1fff660c1e651b6e5934bf6156b4b66e7dc
k-wakisaka <k.wakisaka@vaikobo.com>
2023-11-27 12:59:14 +0900

    first commit

--- model/mobile_net_v2_cam_video_model.py
+++ model/mobile_net_v2_cam_video_model.py
@@ -8,36 +8,13 @@ def prepare(class_num, image_size=320, bottle_neck=64, fine=False):
 
     inputs = tf.keras.Input(shape=(None, image_size, image_size, 3))
     x0 = tf.keras.layers.TimeDistributed(partial_model)(inputs)
-
-    # Block 1
-    ## Resize
-    x0 = tf.keras.layers.Conv3D(filters=bottle_neck//4, kernel_size=(3, 3, 3), strides=(2, 1, 1), padding='same')(x0)
-    ## ResConv2Plus1D
-    x1 = tf.keras.layers.Conv3D(filters=bottle_neck//4, kernel_size=(1, 7, 7), padding='same')(x0)
-    x1 = tf.keras.layers.Conv3D(filters=bottle_neck//4, kernel_size=(3, 1, 1), padding='same')(x1)
-    x1 = tf.keras.layers.BatchNormalization()(x1)
-    x1 = tf.keras.layers.ReLU()(x1)
-    x1 = tf.keras.layers.Conv3D(filters=bottle_neck//4, kernel_size=(1, 7, 7), padding='same')(x1)
-    x1 = tf.keras.layers.Conv3D(filters=bottle_neck//4, kernel_size=(3, 1, 1), padding='same')(x1)
-    x1 = tf.keras.layers.BatchNormalization()(x1)
-    x1 = tf.keras.layers.Add()([x0, x1])
-    # Block 2
-    ## Resize
-    x1 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), strides=(2, 1, 1), padding='same')(x1)
-    ## ResConv2Plus1D
-    x2 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(1, 7, 7), padding='same')(x1)
-    x2 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 1, 1), padding='same')(x2)
-    x2 = tf.keras.layers.BatchNormalization()(x2)
-    x2 = tf.keras.layers.ReLU()(x2)
-    x2 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(1, 7, 7), padding='same')(x2)
-    x2 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 1, 1), padding='same')(x2)
-    x2 = tf.keras.layers.BatchNormalization()(x2)
-    x11 = tf.keras.layers.Dense(units=bottle_neck//2)(x1)
-    x11 = tf.keras.layers.BatchNormalization()(x11)
-    x2 = tf.keras.layers.Add()([x11, x2])
+    x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(1, 1, 1), activation='relu', padding='same')(x0)
+    x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(2, 2, 2), activation='relu', padding='same')(x0)
+    x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(4, 4, 4), activation='relu', padding='same')(x0)
+    x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(8, 8, 8), activation='relu', padding='same')(x0)
     # Block 3
     ## Resize
-    cam_output = tf.keras.layers.Conv3D(filters=class_num, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(x2)
+    cam_output = tf.keras.layers.Conv3D(filters=class_num, kernel_size=(3, 3, 3), dilation_rate=(1, 1, 1), activation='relu', padding='same')(x0)
     predictions = tf.keras.layers.Lambda(lambda x: tf.reduce_sum(x, axis=[1, 2, 3]))(cam_output)
     train_model = tf.keras.Model(inputs=inputs, outputs=predictions)
     save_model = tf.keras.Model(inputs=inputs, outputs=[predictions, cam_output])
--- train_pb.py
+++ train_pb.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     parser.add_argument('--classes_txt_path', type=str, default='/media/kentaro/dataset/.vaik-mnist-video-count-dataset/train_tfrecords/classes.txt')
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--step_size', type=int, default=1000)
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--image_size', type=int, default=224)
     parser.add_argument('--skip_frame_ratio', nargs='+', type=int, default=(1, 2, 4))
     parser.add_argument('--valid_sample_num', type=int, default=100)
