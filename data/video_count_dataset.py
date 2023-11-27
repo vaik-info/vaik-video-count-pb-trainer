4418b2ce0e1109d3f5a3084e94d5c22c7c0c7aa9
k-wakisaka <k.wakisaka@vaikobo.com>
2023-11-27 10:27:15 +0900

    first commit

---
+++ data/video_count_dataset.py
@@ -0,0 +1,82 @@
+import random
+import glob
+import os
+
+import tqdm
+import numpy as np
+import tensorflow as tf
+
+
+class VideoCountDataset:
+    output_signature = None
+    classes = None
+    frame_num = None
+    skip_frame_ratio = None
+    input_size = None
+    max_sample_num = None
+    parsed_dataset = None
+
+    def __new__(cls, tfrecords_dir_path, classes, skip_frame_ratio=(1, 2), input_size=(320, 320, 3), max_sample_num=None):
+        cls.classes = classes
+        cls.skip_frame_ratio = skip_frame_ratio
+        cls.input_size = input_size
+        cls.max_sample_num = max_sample_num
+        cls.parsed_dataset = cls.__load_tfrecords(tfrecords_dir_path)
+        cls.output_signature = (tf.TensorSpec(name=f'video', shape=(None,) + input_size, dtype=tf.uint8),
+                                tf.TensorSpec(name=f'count', shape=(len(classes)), dtype=tf.int32))
+
+        dataset = tf.data.Dataset.from_generator(
+            cls._generator,
+            output_signature=cls.output_signature
+        )
+        return dataset
+
+    @classmethod
+    def _generator(cls):
+        step_index = 0
+        while True:
+            for parsed_data in cls.parsed_dataset:
+                step_index += 1
+                if cls.max_sample_num is not None and cls.max_sample_num < step_index:
+                    return
+                video, count = parsed_data
+                video_array = np.zeros((video.shape[0], ) + cls.input_size, dtype=np.uint8)
+                random_skip_frame_ratio = random.choice(cls.skip_frame_ratio)
+                video = video[::random_skip_frame_ratio]
+                video = tf.image.resize_with_crop_or_pad(video, max(video.shape[1:3]), max(video.shape[1:3]))
+                video = tf.image.resize(video, (cls.input_size[0], cls.input_size[1]))
+                video_array[:video.shape[0], :, :, :] = video
+                yield tf.cast(video_array, tf.uint8), tf.cast(count, tf.int32)
+
+    @classmethod
+    def __parse_tfrecord_fn(cls, example):
+        feature_description = {
+            'video': tf.io.FixedLenFeature([], tf.string),
+            'shape': tf.io.FixedLenFeature([4], tf.int64),
+            'count': tf.io.FixedLenFeature([len(cls.classes)], tf.int64)
+        }
+        example = tf.io.parse_single_example(example, feature_description)
+        video = tf.io.parse_tensor(example['video'], out_type=tf.uint8)
+        shape = example['shape']
+        video = tf.reshape(video, shape)
+        class_index = example['count']
+        return video, class_index
+
+    @classmethod
+    def __load_tfrecords(cls, tfrecords_dir_path):
+        tfrecords_path_list = glob.glob(os.path.join(tfrecords_dir_path, '*.tfrecords-*'))
+        raw_dataset = tf.data.TFRecordDataset(tfrecords_path_list)
+        parsed_dataset = raw_dataset.map(cls.__parse_tfrecord_fn)
+        return parsed_dataset
+
+
+    @classmethod
+    def get_all_data(cls, dataset):
+        dataset = iter(dataset)
+        data_list = []
+        for data in tqdm.tqdm(dataset, desc='get_all_data', total=cls.max_sample_num):
+            data_list.append(data)
+        all_data_list = [None for _ in range(len(data_list[0]))]
+        for index in range(len(data_list[0])):
+            all_data_list[index] = tf.stack([data[index] for data in data_list])
+        return tuple(all_data_list)
---
+++ dump_train_dataset.py
@@ -0,0 +1,52 @@
+import os
+import argparse
+from tqdm import tqdm
+from PIL import Image
+from data import video_count_dataset
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+def create_gif(video, output_file_path, duration=5):
+    gif_image_list = []
+    for video_index in range(video.shape[0]):
+        image = video[video_index].numpy()
+        gif_image_list.append(Image.fromarray(image))
+    gif_image_list[0].save(output_file_path,
+                           save_all=True, append_images=gif_image_list[1:], optimize=False, duration=duration, loop=0)
+
+
+def dump(tfrecords_dir_path, classes_txt_path, sample_num, image_height, image_width, skip_frame_ratio, output_dir_path):
+    os.makedirs(output_dir_path, exist_ok=True)
+    with open(classes_txt_path, 'r') as f:
+        classes = f.readlines()
+    classes = [label.strip() for label in classes]
+
+    DumpDataset = type(f'DumpVideoClassificationDataset', (video_count_dataset.VideoCountDataset,), dict())
+    dump_dataset = DumpDataset(tfrecords_dir_path, classes, skip_frame_ratio=(skip_frame_ratio, ),
+                               input_size=(image_height, image_width, 3), max_sample_num=sample_num)
+
+    for index, data in tqdm(enumerate(dump_dataset)):
+        video, count = data
+        count_str = ""
+        for class_index, label in enumerate(classes):
+            count_str += f'{label}_{count[class_index]}_'
+        count_str = count_str[:-1]
+        output_file_path = os.path.join(output_dir_path, f'{count_str}_skipframe-{skip_frame_ratio}_{index:04d}.gif')
+        create_gif(video, output_file_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='dump')
+    parser.add_argument('--tfrecords_dir_path', type=str, default='/media/kentaro/dataset/.vaik-mnist-video-count-dataset/valid_tfrecords')
+    parser.add_argument('--classes_txt_path', type=str, default='/media/kentaro/dataset/.vaik-mnist-video-count-dataset/valid_tfrecords/classes.txt')
+    parser.add_argument('--sample_num', type=int, default=100)
+    parser.add_argument('--image_height', type=int, default=320)
+    parser.add_argument('--image_width', type=int, default=320)
+    parser.add_argument('--skip_frame_ratio', type=int, default=2)
+    parser.add_argument('--output_dir_path', type=str, default='~/.vaik-video-count-pb-trainer/dump')
+    args = parser.parse_args()
+
+    args.tfrecords_dir_path = os.path.expanduser(args.tfrecords_dir_path)
+    args.classes_txt_path = os.path.expanduser(args.classes_txt_path)
+    args.output_dir_path = os.path.expanduser(args.output_dir_path)
+
+    dump(args.tfrecords_dir_path, args.classes_txt_path, args.sample_num, args.image_height, args.image_width, args.skip_frame_ratio, args.output_dir_path)
