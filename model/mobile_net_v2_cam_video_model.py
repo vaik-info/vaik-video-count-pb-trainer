1dfe8ef4bddcd088f1cce30124bc22c3fb59f4a7
k-wakisaka <k.wakisaka@vaikobo.com>
2023-12-02 10:55:59 +0900

    first commit

--- model/mobile_net_v2_cam_video_model.py
+++ model/mobile_net_v2_cam_video_model.py
@@ -3,10 +3,10 @@ import os.path
 import tensorflow as tf
 
 class LengthTTVLossLayer(tf.keras.layers.Layer):
-    def __init__(self, active_huber_weight=1.0, blank_huber_weight=1.0, ttv_weight=0.01):
+    def __init__(self, active_huber_weight=1.0, blank_huber_weight=1.0, kl_weight=0.1):
         self.active_huber_weight = tf.convert_to_tensor(active_huber_weight, dtype=tf.float32)
         self.blank_huber_weight = tf.convert_to_tensor(blank_huber_weight, dtype=tf.float32)
-        self.ttv_weight = tf.convert_to_tensor(ttv_weight, dtype=tf.float32)
+        self.kl_weight = tf.convert_to_tensor(kl_weight, dtype=tf.float32)
         super(LengthTTVLossLayer, self).__init__()
     def call(self, inputs, *args, **kwargs):
         cam_output, count, length = inputs
@@ -17,27 +17,27 @@ class LengthTTVLossLayer(tf.keras.layers.Layer):
         def process_batch(i):
             active_pred = cam_output[i, :length[i], ...]
             blank_pred = cam_output[i, length[i]:, ...]
-
+            active_uniform_distribution = tf.divide(tf.ones_like(active_pred), tf.cast(tf.shape(active_pred)[1], tf.float32))
             active_pred = tf.pad(active_pred, [[0, max_length - tf.shape(active_pred)[0]], [0, 0], [0, 0], [0, 0]])
+            active_uniform_distribution = tf.pad(active_uniform_distribution, [[0, max_length - tf.shape(active_uniform_distribution)[0]], [0, 0], [0, 0], [0, 0]])
             blank_pred = tf.pad(blank_pred, [[0, max_length - tf.shape(blank_pred)[0]], [0, 0], [0, 0], [0, 0]])
+            return active_pred, active_uniform_distribution, blank_pred
 
-            return active_pred, blank_pred
-
-        active_predictions, blank_predictions = tf.map_fn(
+        active_predictions, active_uniform_distributions, blank_predictions = tf.map_fn(
             process_batch,
             elems=tf.range(batch_size),
-            dtype=(cam_output.dtype, cam_output.dtype),
+            dtype=(cam_output.dtype, cam_output.dtype, cam_output.dtype),
             parallel_iterations=32
         )
 
         active_huber_loss = tf.losses.huber(count, tf.reduce_sum(active_predictions, axis=[1, 2, 3])) * self.active_huber_weight
         blank_huber_loss = tf.losses.huber(0, tf.reduce_sum(blank_predictions, axis=[1, 2, 3])) * self.blank_huber_weight
-        ttv_regularization = 1 / (tf.math.reduce_variance(tf.abs(active_predictions[:, 1:] - active_predictions[:, :-1])) + 1e-6) * self.ttv_weight
-        total_loss = active_huber_loss + blank_huber_loss + ttv_regularization
+        kl_loss = tf.keras.losses.KLDivergence()(active_uniform_distributions, active_predictions) * self.kl_weight
+        total_loss = active_huber_loss + blank_huber_loss + kl_loss
         self.add_loss(total_loss)
         self.add_metric(active_huber_loss, name='active_huber_loss')
         self.add_metric(blank_huber_loss, name='blank_huber_loss')
-        self.add_metric(ttv_regularization, name='ttv_regularization')
+        self.add_metric(kl_loss, name='kl_loss')
 
         return cam_output
 
@@ -56,6 +56,7 @@ def prepare(class_num, image_size=320, bottle_neck=64, pretrain_weight_path=None
     count = tf.keras.layers.Input((class_num), dtype=tf.int32)
     length = tf.keras.layers.Input((), dtype=tf.int32)
     x0 = tf.keras.layers.TimeDistributed(partial_model)(inputs)
+    x0 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(filters=bottle_neck, kernel_size=1, activation='relu', padding='same'))(x0)
     x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(1, 1, 1), activation='relu', padding='same')(x0)
     x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(2, 2, 2), activation='relu', padding='same')(x0)
     x0 = tf.keras.layers.Conv3D(filters=bottle_neck//2, kernel_size=(3, 3, 3), dilation_rate=(4, 4, 4), activation='relu', padding='same')(x0)
